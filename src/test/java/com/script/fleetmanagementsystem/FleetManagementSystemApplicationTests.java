e9b8cf520ce352f0aa9a18d61bc61e655bdab9a0
Leonid Polechshuk <polecleo@cvut.fel.cz>
2026-05-28 17:13:17 +0200

    Initial commit

---
+++ src/test/java/com/script/fleetmanagementsystem/FleetManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.script.fleetmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
